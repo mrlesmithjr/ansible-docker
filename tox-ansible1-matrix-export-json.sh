4b6db306a1c2f6fa2688c61216ab36c14943362d
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2025-03-23 22:24:48 +0800

    Updated tox-ansible matrix export bash script skip cgroup v1

--- tox-ansible1-matrix-export-json.sh
+++ tox-ansible1-matrix-export-json.sh
@@ -11,12 +11,16 @@ json_array=()
 for line in "${lines[@]}"; do
     IFS='-' read -r ansible target <<< "$line"
     if [[ "$line" != *"lint_all"* && "$line" != "python" && "$target" != "python" ]]; then
-        if [[ "$target" == *"@cgroupv1"* ]]; then
+        IFS='@' read -r target cgroup <<< "$target"
+        # skip if cgroup v2 exists
+        if [[ "$cgroup" == "cgroupv1" && " ${lines[*]} " =~ " $ansible-$target@cgroupv2 " ]]; then
+            continue
+        fi
+        if [[ "$cgroup" == "cgroupv1" ]]; then
             run_on="ubuntu-20.04"
         else
             run_on="ubuntu-22.04"
         fi
-        IFS='@' read -r target cgroup <<< "$target"
         python_version=$(cat tox.ini | grep "$ansible: python" | awk -F 'python' '{print $2}')
         json_obj="{\"name\": \"$line\", \"run_on\": \"$run_on\", \"python_version\": \"$python_version\", \"conf\": \"tox.ini\", \"factors\": [\"python$python_version\", \"$ansible\", \"$target\", \"$cgroup\"]}"
         json_array+=("$json_obj")
