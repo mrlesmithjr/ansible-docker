6399bb8fbdc15c4f5bcc6d59c0c49d8477a60b52
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2025-03-23 22:24:41 +0800

    Added tox-ansible matrix export bash script

---
+++ tox-ansible1-matrix-export-json.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+lines=()
+
+while IFS= read -r line; do
+    lines+=("$line")
+done < <(tox -c tox.ini -l 2>/dev/null)
+
+json_array=()
+
+for line in "${lines[@]}"; do
+    IFS='-' read -r ansible target <<< "$line"
+    if [[ "$line" != *"lint_all"* && "$line" != "python" && "$target" != "python" ]]; then
+        if [[ "$target" == *"@cgroupv1"* ]]; then
+            run_on="ubuntu-20.04"
+        else
+            run_on="ubuntu-22.04"
+        fi
+        IFS='@' read -r target cgroup <<< "$target"
+        python_version=$(cat tox.ini | grep "$ansible: python" | awk -F 'python' '{print $2}')
+        json_obj="{\"name\": \"$line\", \"run_on\": \"$run_on\", \"python_version\": \"$python_version\", \"conf\": \"tox.ini\", \"factors\": [\"python$python_version\", \"$ansible\", \"$target\", \"$cgroup\"]}"
+        json_array+=("$json_obj")
+    fi
+done
+
+printf "[%s]\n" "$(IFS=,; echo "${json_array[*]}")" | jq .
