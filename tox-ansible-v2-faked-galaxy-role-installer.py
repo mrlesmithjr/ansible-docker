b3b6b9d8ceb91bc34aa78487060a56765fd85319
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2025-03-24 20:06:16 +0800

    Fixed ansible-compat cache path when >= 25.0.0

--- tox-ansible-v2-faked-galaxy-role-installer.py
+++ tox-ansible-v2-faked-galaxy-role-installer.py
@@ -1,13 +1,21 @@
 """Install the role project when Tox Ansible v2 encounters a faked galaxy.yml."""
+
 from ansible_compat.runtime import Runtime
 from packaging.version import Version
+import ansible_compat
 
 
-runtime = Runtime(isolated=True)
+# fix ansible-compat >= 25.0.0 cache_dir cant load collection
+# if >= 25.0.0, isolated will `project_dir/.ansible` not `~/.cache/ansible-compat/`
+isolated = Version(ansible_compat.__version__) < Version("25.0.0")
+runtime = Runtime(isolated=isolated)
 runtime._install_galaxy_role(project_dir=runtime.project_dir, role_name_check=2)
 
 v210 = Version("2.10.0")
 if runtime.version < v210:
     # fix ansible 2.9 need community.docker < 3.0.0
     # see https://github.com/ansible-collections/community.docker/issues/477
-    runtime.install_collection(f"community.docker:<3.0.0")
+    destination = f"{runtime.cache_dir}/collections"
+    runtime.install_collection(
+        collection="community.docker:<3.0.0", destination=destination
+    )
