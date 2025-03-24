28d553ed384b20192cc9e67ce3c7907ef3d6a5c1
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2025-03-24 20:05:26 +0800

    Updated tox-ansible bash script for tox-ansible v2

--- tox-ansible.sh
+++ tox-ansible.sh
@@ -25,70 +25,36 @@ for arg in "$@"; do
     esac
 done
 
-environments=()
-
-while IFS= read -r line; do
-    environments+=("$line")
-done < <(tox -c tox.ini -l 2>/dev/null)
-
 enable_environments=()
 
-for environment in "${environments[@]}"; do
-    skip=$(grep "skip.environment.$environment" tox.ini)
-    if [[ -n "$skip" ]]; then
-        continue
-    fi
-
-    IFS='-' read -r ansible scenario <<< "$environment"
-    skip=$(grep "skip.scenario.$scenario" tox.ini)
-    if [[ -n "$skip" ]]; then
-        continue
-    fi
-
-    IFS='@' read -r target cgroup <<< "$scenario"
-    skip=$(grep "skip.ansible-target.$ansible-$target" tox.ini)
-    if [[ -n "$skip" ]]; then
-        continue
-    fi
-
-    enable_environments+=("$environment")
-done
+while IFS= read -r line; do
+    enable_environments+=("$line")
+done < <(tox --ansible -c tox-ansible.ini -l 2>/dev/null)
 
 json_array=()
 
 for environment in "${enable_environments[@]}"; do
-    IFS='-' read -r ansible scenario <<< "$environment"
-    IFS='@' read -r target cgroup <<< "$scenario"
-
-    if [[ "$INCLUDE_CGROUP" == "auto" && "$cgroup" == "cgroupv1" && "${enable_environments[*]}" =~ "$ansible-$target@cgroupv2" ]]; then
-        # skip if cgroup v2 exists skip cgroupv1
-        continue
-    elif [[ "$INCLUDE_CGROUP" == "v1" && "$cgroup" == "cgroupv2" ]]; then
-        # skip cgroupv2
-        continue
-    elif [[ "$INCLUDE_CGROUP" == "v2" && "$cgroup" == "cgroupv1" ]]; then
-        # skip cgroupv1
-        continue
+    IFS='-' read -r factor py ansible <<< "$environment"
+    python_version=$(echo "$py" | awk -F 'py' '{print $2}')
+    cgroups=()
+    if [[ "$INCLUDE_CGROUP" == "auto" ]]; then
+        cgroups+=("cgroupv1")
+        cgroups+=("cgroupv2")
+    elif [[ "$INCLUDE_CGROUP" == "v1" ]]; then
+        cgroups+=("cgroupv1")
+    elif [[ "$INCLUDE_CGROUP" == "v2" ]]; then
+        cgroups+=("cgroupv2")
     fi
 
-    # override run_on
-    run_on=$(grep "set.cgroup.$cgroup.run_on" tox.ini | awk -F ' = ' '{print $2}')
-    if [[ -z "$run_on" ]]; then
+    for cgroup in "${cgroups[@]}"; do
         if [[ "$cgroup" == "cgroupv1" ]]; then
             run_on="ubuntu-20.04"
         else
             run_on="ubuntu-22.04"
         fi
-    fi
-
-    # override python_version
-    python_version=$(grep "set.ansible.$ansible.python_version" tox.ini | awk -F ' = ' '{print $2}')
-    if [[ -z "$python_version" ]]; then
-        python_version=$(grep "$ansible: python" tox.ini | awk -F 'python' '{print $2}')
-    fi
-
-    json_obj="{\"name\": \"$target-$ansible($cgroup-$python_version)\", \"run_on\": \"$run_on\", \"python_version\": \"$python_version\", \"conf\": \"tox.ini\", \"environment\": \"$environment\", \"factors\": [\"python$python_version\", \"$ansible\", \"$target\", \"$cgroup\"]}"
-    json_array+=("$json_obj")
+        json_obj="{\"name\": \"ansible$ansible(py$python_version)@$run_on\", \"cgroup\": \"$cgroup\", \"run_on\": \"$run_on\", \"python_version\": \"$python_version\", \"conf\": \"tox-ansible.ini\", \"environment\": \"$environment\", \"factors\": [\"python$python_version\", \"ansible$ansible\"]}"
+        json_array+=("$json_obj")
+    done
 done
 
 if [[ "$STDOUT_JSON" == "true" ]]; then
@@ -98,6 +64,7 @@ else
     for json_obj in "${json_array[@]}"; do
         conf=$(echo "$json_obj" | jq -r '.conf')
         env=$(echo "$json_obj" | jq -r '.environment')
-        tox -c "$conf" -e "$env" -v
+        cgroup=$(echo "$json_obj" | jq -r '.cgroup')
+        MATCH_MOLECULE_CGROUP=$cgroup tox --ansible -c "$conf" -e "$env" -v
     done
 fi
