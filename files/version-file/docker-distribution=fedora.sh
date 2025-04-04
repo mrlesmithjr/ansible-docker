5179d812a622c42d7b8542e76e0c66f5f699809b
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2025-04-04 21:01:33 +0800

    Added bash script for generating Fedora packages version JSON

---
+++ files/version-file/docker-distribution=fedora.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+script_path=$(dirname "$(realpath "$0")")
+script_name=$(basename "$0")
+script_name_no_ext="${script_name%.*}"
+
+STDOUT_JSON=false
+
+for arg in "$@"; do
+    case "$arg" in
+        --stdout)
+            STDOUT_JSON=true
+            ;;
+        *)
+            echo "unknow: $arg"
+            echo "usage: $0 [--stdout]"
+            exit 1
+            ;;
+    esac
+done
+
+json_array=()
+
+os_name=fedora
+paths=()
+paths+=("24/x86_64")
+paths+=("27/aarch64")
+paths+=("27/x86_64")
+paths+=("29/aarch64")
+paths+=("29/x86_64")
+paths+=("30/aarch64")
+paths+=("30/x86_64")
+paths+=("31/aarch64")
+paths+=("31/x86_64")
+paths+=("32/aarch64")
+paths+=("32/ppc64le")
+paths+=("32/x86_64")
+paths+=("33/aarch64")
+paths+=("33/ppc64le")
+paths+=("33/x86_64")
+paths+=("34/aarch64")
+paths+=("34/ppc64le")
+paths+=("34/x86_64")
+paths+=("35/aarch64")
+paths+=("35/ppc64le")
+paths+=("35/x86_64")
+paths+=("36/aarch64")
+paths+=("36/ppc64le")
+paths+=("36/x86_64")
+paths+=("37/aarch64")
+paths+=("37/ppc64le")
+paths+=("37/x86_64")
+paths+=("38/aarch64")
+paths+=("38/ppc64le")
+paths+=("38/x86_64")
+paths+=("39/aarch64")
+paths+=("39/ppc64le")
+paths+=("39/x86_64")
+paths+=("40/aarch64")
+paths+=("40/ppc64le")
+paths+=("40/x86_64")
+paths+=("41/aarch64")
+paths+=("41/ppc64le")
+paths+=("41/x86_64")
+
+for path in "${paths[@]}"; do
+    IFS='/' read -r os_version os_arch <<< "$path"
+
+    while IFS= read -r line; do
+        if [[ "$line" =~ ^docker-ce-[0-9]+ ]]; then
+            package_version="${line#docker-ce-}"
+            package_version="${package_version%.rpm}"
+            package_version="${package_version%.$os_arch}"
+            package_version="${package_version%.$os_version}"
+
+            IFS='-' read -r docker_version p <<< "$package_version"
+            docker_version="${docker_version%.ce}"
+
+            json_obj="{\"docker_version\": \"$docker_version\", \"package_version\": \"$package_version\", \"os_name\": \"$os_name\", \"os_version\": \"$os_version\", \"os_arch\": \"$os_arch\"}"
+            json_array+=("$json_obj")
+        fi
+    done < <(curl -s https://download.docker.com/linux/$os_name/$os_version/$os_arch/stable/Packages/ | grep -oP 'href="\K[^"]+' | sort)
+done
+
+if [[ "$STDOUT_JSON" == "true" ]]; then
+    printf "[%s]\n" "$(IFS=,; echo "${json_array[*]}")" | jq 'sort_by(.name)'
+else
+    printf "[%s]\n" "$(IFS=,; echo "${json_array[*]}")" | jq 'sort_by(.name)' > $script_path/$script_name_no_ext.json
+fi
